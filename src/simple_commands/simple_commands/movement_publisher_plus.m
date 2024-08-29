17ed4c361f5cf3609404f4074c19d4b9690538cc
SantiP13 <s.penunuri@hotmail.com>
2024-08-28 19:35:34 -0600

    Joint States comparison

--- src/simple_commands/simple_commands/movement_publisher_plus.m
+++ src/simple_commands/simple_commands/movement_publisher_plus.m
@@ -1,16 +1,27 @@
+% Comando Inicializar Camara del Turtlebot:
+% ros2 run v4l2_camera v4l2_camera_node --ros-args -p image_size:="[320,240]" -p framerate:=30 --ros-args --param reliability:=best_effort --param history:=keep_last --param depth:=10
+
 
 movementNode = ros2node('/movement_publisher_node');
 
 twistPub = ros2publisher(movementNode,'/cmd_vel','geometry_msgs/Twist');
 twistPubMsg = ros2message(twistPub);
 
-jointStateSub = ros2subscriber(movementNode, '/joint_states');
-jointStateSubMsg = ros2message(jointStateSub);
-[scanData, status, statusText] = receive(jointStateSub);
+jointStateSub = ros2subscriber(movementNode, '/joint_states', 'sensor_msgs/JointState', @readJointStates);
+global v_r
+global w_r
 
 imageSub = ros2subscriber(movementNode,'/image_raw', 'sensor_msgs/Image');
 imageSubMsg = ros2message(imageSub);
 
+function readJointStates(msg)
+    global v_r
+    global w_r
+    v_r = (msg.velocity(1) + msg.velocity(2)) / 2;
+    w_r = (msg.velocity(1) - msg.velocity(2)) / 2;
+end
+
+% Estado del robot 
 x = 0;
 y = 0;
 theta = 0;
@@ -32,7 +43,6 @@ yd = 0.5;
 vMax = 0.18;
 wMax = 0.4;
 
-
 tic
 while t < tf
    thetad = atan2((yd-y), (xd- x));
@@ -40,35 +50,49 @@ while t < tf
    d = sqrt( (x-xd)^2 + (y-yd)^2 );
    thetae = theta - thetad;
 
+   %% Condicional para tomar rotación más corta
    if thetae > pi
        thetae = thetae - 2*pi;
    elseif thetae <= -pi
        thetae = thetae + 2*pi;
    end
 
+   %% Condicional para reversa
+   if thetae > pi/2
+       thetae = thetae - pi;
+       d = -d;
+   elseif thetae < - pi/2
+       thetae = thetae + pi;
+       d = -d;
+   end
+
+   %% Condicional para avanzar cuando este orientado
    if thetae < pi/32 && thetae > -pi/32
-        v = kpd * d;
+        v = kpd * d + kpd * (v - v_r);
         v = vMax * tanh(v / vMax);
    else
        v = 0;
    end
 
-   w = -kpr * thetae;
+   w = -kpr * thetae + kpr * (w-w_r);
    w = wMax * tanh(w / wMax);
-
+   %% Area Objetivo
    if d < 0.02 
        v = 0;
        w = 0;
-       
    end
 
+   %% Calculo de derivadas
    x_dot = v * cos(theta);
    y_dot = v * sin(theta);
    theta_dot = w;
 
+   %% Publicación de velocidades al robot
    twistPubMsg.linear.x = v;
    twistPubMsg.linear.y = 0.0;
    twistPubMsg.angular.z = theta_dot;
+
+   send(twistPub,twistPubMsg);
 
    % twistPubMsg.linear.x = 0;
    % twistPubMsg.linear.y = 0;
@@ -77,8 +101,6 @@ while t < tf
    x = x + x_dot * dt;
    y = y + y_dot * dt;
    theta = theta + theta_dot*dt;
-
-   send(twistPub,twistPubMsg);
 
    dt = toc;
    tic
